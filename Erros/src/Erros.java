bec8a24a74e885ef533798a5592e9d499c8da4ac
Rosonatt Ferreira <h2oroso@gmail.com>
2024-12-17 14:09:10 -0300

    att

--- Erros/src/Erros.java
+++ Erros/src/Erros.java
@@ -4,6 +4,12 @@ public class Erros {
 
         //2 tipos de erros
         //erro de compilação
-        double number1 = 10.45;
+        String text = "ola mundo";
+        System.out.println(text);
+
+        //erro de execução
+        int x = 10;
+        int y = 0;
+        System.out.println(x/y);
     }
 }
