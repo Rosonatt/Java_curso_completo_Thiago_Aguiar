d29c83749eebf09acdf4b9c636162ad1b38b23f1
Rosonatt Ferreira <h2oroso@gmail.com>
2024-12-18 19:38:07 -0300

    att

---
+++ Interacoes/src/interacoes.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+import static java.util.Scanner.*;
+
+public class interacoes {
+
+    public static void main(String[] args){
+        Scanner scanner  = new Scanner(System.in);
+
+        System.out.println("Digite o seu nome: ");
+        String name = scanner.nextLine();
+
+        System.out.println("ola, " + name + " seja bem vindo" );
+
+
+
+    }
+
+}
