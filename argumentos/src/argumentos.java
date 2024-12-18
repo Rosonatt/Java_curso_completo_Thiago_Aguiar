151732c43afecf4239e70457aa4e44444cfe1534
Rosonatt Ferreira <h2oroso@gmail.com>
2024-12-18 18:46:35 -0300

    att

---
+++ argumentos/src/argumentos.java
@@ -0,0 +1,12 @@
+public class argumentos {
+
+    public static void main(String[] args) {
+     System.out.println( "ola" + " " + args[0]);
+     System.out.println( "#" + " " + args[1]);
+     System.out.println( "#" + " " + args[2]);
+
+
+
+
+    }
+}
